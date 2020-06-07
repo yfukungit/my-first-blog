c5cabe05454af91a4d1371be154602fc63bf71b7
Y.FUKU <ilsoccer902@yahoo.co.jp>
2020-06-07 17:04:38 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,9 @@ from django.utils import timezone
 
 #https://tutorial.djangogirls.org/ja/django_models/
 class Post(models.Model):
-    #これは他のモデルへのリンク
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    #文字数が制限されたテキストを定義するフィールド
     title = models.CharField(max_length=200)
-    #これは制限無しの長いテキスト用です。ブログポストのコンテンツに理想的なフィールドでしょ？   
     text = models.TextField()
-    #日付と時間のフィールド
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
